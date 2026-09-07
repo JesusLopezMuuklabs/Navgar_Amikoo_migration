ab99ce6ea23a28e0bc0ea745bc65af76aae4b646
Jesus Lopez <jesus.lopez@muuklabs.com>
2026-09-06 20:50:53 -0600

    Add files via upload

---
+++ muuk-tests/Tasks_Module/PageDetails.ts
@@ -0,0 +1,550 @@
+import { expect, FrameLocator, Frame, Locator, Page } from '@playwright/test';
+import MuukLocator from './MuukLocator';
+//import utils from Utils;
+
+export class dashboardStagingNavgod25yq9dx43d {
+  page: Page;
+  h2VKUvVlcxo9Field: MuukLocator = {
+    index: 0,
+    selector: `//a[contains(text(),\"Log In\")]`,
+  }
+  inputaPC0oahLxOField: MuukLocator = {
+    index: 0,
+    selector: `INPUT[type='email'][name='user[email]'][id='user_email']`,
+  }
+  inputNoTndoCpdqField: MuukLocator = {
+    index: 0,
+    selector: `INPUT[type='password'][name='user[password]'][id='user_password']`,
+  }
+  inputPriEV9ualJField: MuukLocator = {
+    index: 0,
+    selector: `INPUT[type='submit'][name='commit']`,
+  }
+}
+export class dashboardStagingNavg8mqlmcoxvr5 {
+  page: Page;
+  pnvhxZqSB3Field: MuukLocator = {
+    index: 0,
+    selector: `//div[contains(text(),\"No rows\")]`,
+  }
+  inputL7LC7xjC0LdField: MuukLocator = {
+    index: 0,
+    selector: `//input[@placeholder=\"Search\"]`,
+  }
+  pnNoc66pSABEField: MuukLocator = {
+    index: 0,
+    selector: `//a[@href=\"/104/tasks?filter=me-to-others\"]`,
+  }
+  svgdmC7rVHpzField: MuukLocator = {
+    index: 0,
+    selector: `//span[normalize-space() = \"Reassign\"]`,
+  }
+  inputwlHKWMcYEXField: MuukLocator = {
+    index: 0,
+    selector: `//input[@name='legend' and @type='text' and @placeholder='Create new task']`,
+  }
+  buttonrWsdWeUVlKField: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[@type='submit'][contains(text(),\"Create\")]`,
+  }
+  pwvhvowiWmoField: MuukLocator = {
+    index: 0,
+    selector: `//div[@role=\"gridcell\"][@data-field=\"legend\"]/a/span[contains(text(), \"Task\")]`,
+  }
+  pXvuB2EHaeField: MuukLocator = {
+    index: 0,
+    selector: `//div[@role='gridcell'][@data-field='legend']//span[text()='Task']`,
+  }
+  spanLXH73Kuq9dField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[contains(text(),\"Add date\")]`,
+  }
+  buttonLes7Xbc5MZField: MuukLocator = {
+    index: 0,
+    selector: `//button[contains(@class, \"MuiPickersDay-today\")]`,
+  }
+  buttonxKul1xHPNxField: MuukLocator = {
+    index: 0,
+    selector: `//button[contains(@class, \"MuiPickersDay-today\")]/following::button`,
+  }
+  spanp8F4XUdZLField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[contains(text(),\"Tomorrow\")]`,
+  }
+  button5yTaoYc28NField: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[@type='button'][normalize-space() = \"OK\"]`,
+  }
+  spanrNwSd7xTwjField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[contains(text(),\"Tasks\")]`,
+  }
+  spaneRMieykU3jField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[contains(text(),\"All\")]`,
+  }
+  span00CAVbFbPZField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[contains(text(),\"Assigned by others\")]`,
+  }
+  divGJFvzUvn8Field: MuukLocator = {
+    index: 0,
+    selector: `//div[@data-field=\"deadline_at\"][@role=\"gridcell\"]`,
+  }
+  pPJcYjt980Field: MuukLocator = {
+    index: 0,
+    selector: `//p[contains(@class, \"MuiTablePagination-displayedRows\")]`,
+  }
+  inputNrSWOYhl8vField: MuukLocator = {
+    index: 0,
+    selector: `INPUT[type='checkbox']`,
+  }
+}
+export class dashboardStagingNavgn9e8i02fzig {
+  page: Page;
+  buttonAMZhoVqtYZ0Field: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[@type='button'][normalize-space() = \"Complete\"]`,
+  }
+  inputZKet5tYnField: MuukLocator = {
+    index: 0,
+    selector: `INPUT[type='checkbox']`,
+  }
+}
+export class dashboardStagingNavgsvgjf9u9ic6c {
+  page: Page;
+  spanWW0sywsNkField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[contains(text(),\"Tasks\")]`,
+  }
+}
+export class dashboardStagingNavgwnry5jsnivb {
+  page: Page;
+  liE6SRE5So64Field: MuukLocator = {
+    index: 0,
+    selector: `//span[contains(text(), \"Tasks\")]`,
+  }
+}
+export class dashboardStagingNavghxs0yt3422ul {
+  page: Page;
+  aYvNFWe5f75BField: MuukLocator = {
+    index: 0,
+    selector: `//A[contains(text(),\"www.google.com\")]`,
+  }
+}
+export class dashboardStagingNavg2yfrgcoatmj {
+  page: Page;
+  textareaVvBfKWXTO3VField: MuukLocator = {
+    index: 0,
+    selector: `//p[@data-placeholder=\"Insert comments here\"]`,
+  }
+}
+export class dashboardStagingNavg5f05sqbgwy6o {
+  page: Page;
+  li7KIVKECwjzField: MuukLocator = {
+    index: 0,
+    selector: `//li[normalize-space() = \"Delete\"]`,
+  }
+}
+export class dashboardStagingNavgf3aog1gy5mu {
+  page: Page;
+  svgVGUmkjUkGYField: MuukLocator = {
+    index: 1,
+    selector: `//div[@data-field=\"deadline_at\"]`,
+  }
+  svgErt1jxC7abField: MuukLocator = {
+    index: 0,
+    selector: `//div[@data-field='actions']//button`,
+  }
+}
+export class dashboardStagingNavgiu4m1aiovm79 {
+  page: Page;
+  div7vUx9dIQBVField: MuukLocator = {
+    index: 1,
+    selector: `//DIV[normalize-space() = \"Task\"]`,
+  }
+  ajjcgARfW49Field: MuukLocator = {
+    index: 0,
+    selector: `//A[normalize-space() = \"Task\"]`,
+  }
+  a6ahz9JHprgField: MuukLocator = {
+    index: 0,
+    selector: `//div[@role=\"gridcell\"][@data-field=\"legend\"]/a/span[normalize-space() = \"Task\"]`,
+  }
+  buttonezSgGKH7nEField: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[@type='button'][normalize-space() = \"New Task\"]`,
+  }
+  buttonQOFlRcZgdField: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[@type='submit'][normalize-space() = \"Create\"]`,
+  }
+  inputeQu63vFPBeField: MuukLocator = {
+    index: 0,
+    selector: `INPUT[name='legend'][placeholder='Create new task'][type='text']`,
+  }
+  span8WfvJf9NzField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[normalize-space() = \"Today\"]`,
+  }
+  spanJRSXaxhMJJField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[normalize-space() = \"Upcoming\"]`,
+  }
+  spanPQugJWh0KYField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[normalize-space() = \"Someday\"]`,
+  }
+  buttonAohaptvWqField: MuukLocator = {
+    index: 0,
+    selector: `(//button[contains(@class,'MuiPickersDay-today')]/following::button[contains(@class,'MuiPickersDay-root')])[1]`,
+  }
+  divRDu5Nqhr8yField: MuukLocator = {
+    index: 2,
+    selector: `//DIV[normalize-space() = \"Task has been assigned to work on Today.\"]`,
+  }
+  divJNf3W6SGx3Field: MuukLocator = {
+    index: 2,
+    selector: `//DIV[normalize-space() = \"Task has been assigned to work on Upcoming.\"]`,
+  }
+  divJKTBVlfbDoField: MuukLocator = {
+    index: 2,
+    selector: `//DIV[normalize-space() = \"Task has been assigned to work on Someday.\"]`,
+  }
+  divQ4sZ98hLkoField: MuukLocator = {
+    index: 4,
+    selector: `//DIV[normalize-space() = \"Task Name\"]`,
+  }
+  divodF3e7FNYYField: MuukLocator = {
+    index: 4,
+    selector: `//DIV[normalize-space() = \"Due Date\"]`,
+  }
+  divD0FllJhKj4Field: MuukLocator = {
+    index: 4,
+    selector: `//DIV[normalize-space() = \"Owner\"]`,
+  }
+  divkGtEyiDoPField: MuukLocator = {
+    index: 4,
+    selector: `//DIV[normalize-space() = \"Created by\"]`,
+  }
+  h62VULllfGWnField: MuukLocator = {
+    index: 0,
+    selector: `//h6[normalize-space() = \"Delete Tasks\"]`,
+  }
+  buttonj3SPqIMYYField: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[@type='button'][normalize-space() = \"Yes, delete this task\"]`,
+  }
+}
+export class dashboardStagingNavgzvlsv9j18z7 {
+  page: Page;
+  buttonkzUlKlTw4VField: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[@type='button'][normalize-space() = \"Select Tasks\"]`,
+  }
+  inputypFXk1uLgGField: MuukLocator = {
+    index: 0,
+    selector: `INPUT[name='select_all_rows'][type='checkbox']`,
+  }
+}
+export class dashboardStagingNavgllgtujbllz93 {
+  page: Page;
+  buttonWkUxNmc7g9Field: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[@type='button'][normalize-space() = \"New Task\"]`,
+  }
+  inputO5I1XMSOkbField: MuukLocator = {
+    index: 0,
+    selector: `//INPUT[@placeholder='Create new task']`,
+  }
+  buttonKdCVZaSEBnField: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[@type='submit'][normalize-space() = \"Create\"]`,
+  }
+  aaJczygFomMField: MuukLocator = {
+    index: 0,
+    selector: `//A[normalize-space() = \"Task\"]`,
+  }
+  button9VcaW5C2bYField: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[@type='button'][normalize-space() = \"Today\"]`,
+  }
+  divmQuMQQulB6Field: MuukLocator = {
+    index: 2,
+    selector: `//DIV[normalize-space() = \"Task has been assigned to work on Today.\"]`,
+  }
+  button8ncYqVH9QField: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[@type='button'][normalize-space() = \"Upcoming\"]`,
+  }
+  buttonVbHBzPuqOwField: MuukLocator = {
+    index: 0,
+    selector: `(//button[contains(@class,'MuiPickersDay-today')]/following::button[contains(@class,'MuiPickersDay-root')])[1]`,
+  }
+  divwwUlpHK5Field: MuukLocator = {
+    index: 2,
+    selector: `//DIV[normalize-space() = \"Task has been assigned to work on Upcoming.\"]`,
+  }
+}
+export class dashboardStagingNavgbdkxq4v1rz6e {
+  page: Page;
+  button6MSbbeTNVnField: MuukLocator = {
+    index: 0,
+    selector: `//button[@aria-label=\"Edit task name\"]`,
+  }
+  spanozUDR2CNgVField: MuukLocator = {
+    index: 1,
+    selector: `//SPAN[normalize-space() = \"Task_edited\"]`,
+  }
+  buttonGgs0MjsG4Field: MuukLocator = {
+    index: 0,
+    selector: `//div[@role='separator']/following-sibling::button`,
+  }
+  inputD1tnyZu8mwField: MuukLocator = {
+    index: 2,
+    selector: `INPUT[type='text']`,
+  }
+}
+export class dashboardStagingNavgwpww231kx1yh {
+  page: Page;
+  buttondXUXkmI0NKField: MuukLocator = {
+    index: 0,
+    selector: `//button[@aria-label='Someday']/following-sibling::button[1]`,
+  }
+  spanW0IJq3IJnqField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[normalize-space() = \"Assign to a project\"]`,
+  }
+  divYahGdGiwnField: MuukLocator = {
+    index: 0,
+    selector: `//input[@placeholder=\"Search a project\"]`,
+  }
+  spanE5snriY4AField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[normalize-space() = \"Snooze\"]`,
+  }
+  div5YHox3FWjQField: MuukLocator = {
+    index: 0,
+    selector: `//div[contains(@class, \"MuiDateCalendar-root\")]`,
+  }
+  buttonDYFkWPUAQeField: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[@type='button'][normalize-space() = \"OK\"]`,
+  }
+}
+export class dashboardStagingNavglgmzb6kl603 {
+  page: Page;
+  button0lSK2Nt9PPField: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[@type='button'][@role='tab'][normalize-space() = \"Details\"]`,
+  }
+}
+export class dashboardStagingNavgsquoemce49i9 {
+  page: Page;
+  spanRotl6fYM8Field: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[normalize-space() = \"+ Add description\"]`,
+  }
+  divgnFd1LIqNpField: MuukLocator = {
+    index: 0,
+    selector: `//p[@data-placeholder=\"+ Add description\"]`,
+  }
+  buttonYwT0mRUmoWField: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[@type='button'][normalize-space() = \"Save\"]`,
+  }
+  pBpcWl3u4NField: MuukLocator = {
+    index: 1,
+    selector: `//P[normalize-space() = \"New description\"]`,
+  }
+  spanx3FODztj2OField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[normalize-space() = \"Set due date\"]`,
+  }
+  spanRLF7NDSdWField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[normalize-space() = \"Reassign\"]`,
+  }
+  spanmKcjNvfRNPField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[normalize-space() = \"VV Victor Villa\"]`,
+  }
+}
+export class dashboardStagingNavgtnd2ciy9trjc {
+  page: Page;
+  aPMckg3SAnField: MuukLocator = {
+    index: 0,
+    selector: `//span//child::a`,
+  }
+  a6mIrlYi1W9Field: MuukLocator = {
+    index: 3,
+    selector: `//span//child::a`,
+  }
+}
+export class dashboardStagingNavgq4t68r1ov3g {
+  page: Page;
+  axdUgx3UYglField: MuukLocator = {
+    index: 2,
+    selector: `//span//child::a`,
+  }
+}
+export class dashboardStagingNavgzkdhpabe4he {
+  page: Page;
+  aLgSZvEIK9AField: MuukLocator = {
+    index: 1,
+    selector: `//span//child::a`,
+  }
+}
+export class dashboardStagingNavgxbd4woeujd2i {
+  page: Page;
+  spangxWHrVLFZwField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[contains(text(),\"Accounts\")]`,
+  }
+  spany8XCnwid2ZField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[contains(text(),\"MuukTest - Feature Flag Environment\")]`,
+  }
+}
+export class dashboardStagingNavgoaunhfltkfj {
+  page: Page;
+  azZS6BauWnField: MuukLocator = {
+    index: 0,
+    selector: `//A[normalize-space() = \"Project\"]`,
+  }
+  divkMs7VdFNAoField: MuukLocator = {
+    index: 0,
+    selector: `//DIV[normalize-space() = \"Angel RamirezTask Assigner\"]`,
+  }
+  divoPsQ9uVexField: MuukLocator = {
+    index: 0,
+    selector: `//DIV[normalize-space() = \"Task Assigner\"]`,
+  }
+  divVOs0b9ilNQField: MuukLocator = {
+    index: 31,
+    selector: `//DIV[normalize-space() = \"AR\"]`,
+  }
+  divG0UYddckMmField: MuukLocator = {
+    index: 28,
+    selector: `//DIV[normalize-space() = \"ARAngel Ramirez\"]`,
+  }
+  buttonwrUX1HwXzField: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[@type='button'][normalize-space() = \"Add\"]`,
+  }
+  pDbU7aDCOjOField: MuukLocator = {
+    index: 0,
+    selector: `//P[normalize-space() = \"MuukTest - Production Environment (All Company)\"]`,
+  }
+  divlOC39DcJrxField: MuukLocator = {
+    index: 0,
+    selector: `//DIV[normalize-space() = \"Tanner Dawson\"]`,
+  }
+  buttonlAHnlPSzN1Field: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[@type='button'][normalize-space() = \"Members\"]`,
+  }
+  buttonOoIGZ4ULQField: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[@type='button'][normalize-space() = \"Remove\"]`,
+  }
+}
+export class dashboardStagingNavgo3hyuoxfqztq {
+  page: Page;
+  buttonavTaEwUA72Field: MuukLocator = {
+    index: 120,
+    selector: `BUTTON[type='button']`,
+  }
+}
+export class dashboardStagingNavgcn67p2et5eda {
+  page: Page;
+  divw1Srv7fwEWField: MuukLocator = {
+    index: 0,
+    selector: `//div[contains(@class, 'MuiTypography-root MuiTypography-subtitle1')]//span[contains(@class, 'MuiBox-root')]`,
+  }
+}
+export class dashboardStagingNavggommsarcfz {
+  page: Page;
+  buttonyAt0XgIDkYField: MuukLocator = {
+    index: 0,
+    selector: `//button[@aria-label='Someday']/following-sibling::button`,
+  }
+  span15I0rUe86Field: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[normalize-space() = \"Help from Navgar AI\"]`,
+  }
+  divMVFklQSXOOField: MuukLocator = {
+    index: 0,
+    selector: `//DIV[@id='rich-text-input-184072'][normalize-space() = \"@NavgarAI Can you help me with the task \"Task\"?\"]`,
+  }
+  spanpvUy0NsrxXField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[normalize-space() = \"@NavgarAI\"]`,
+  }
+  spanmQCndvso4GField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[normalize-space() = \"Reassign\"]`,
+  }
+  divRLfJZaipYField: MuukLocator = {
+    index: 0,
+    selector: `//input[@placeholder=\"Search\"]//ancestor::div[contains(@class, \"MuiPaper-root\")]`,
+  }
+  spanKmiyGehvBJField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[normalize-space() = \"Set due date\"]`,
+  }
+  divk7T0wouVxVField: MuukLocator = {
+    index: 0,
+    selector: `//div[contains(@class, \"MuiDateCalendar-root\")]`,
+  }
+  span4QIDOgt5jnField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[normalize-space() = \"Tags\"]`,
+  }
+  div4oi6kdFj8pField: MuukLocator = {
+    index: 3,
+    selector: `//DIV[normalize-space() = \"Tag#1\"]`,
+  }
+  div1QFd5yfkjField: MuukLocator = {
+    index: 3,
+    selector: `//DIV[normalize-space() = \"Tag#2\"]`,
+  }
+  spanGJulbieOJField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[normalize-space() = \"Assign to a project\"]`,
+  }
+  buttonRWT9wptPQField: MuukLocator = {
+    index: 0,
+    selector: `//input[@placeholder=\"Search a project\"]//ancestor::div[contains(@class, \"MuiPaper-root\")]`,
+  }
+  spanrmuz4SAbaField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[normalize-space() = \"Assign to entity\"]`,
+  }
+  div3WU99Dc7QXField: MuukLocator = {
+    index: 0,
+    selector: `//DIV[normalize-space() = \"Select Entity Type\"]`,
+  }
+  div1mUxQqTWeYField: MuukLocator = {
+    index: 0,
+    selector: `//p[normalize-space() = \"Select Entity Type\"]//ancestor::div[contains(@class, \"MuiPaper-root\")]`,
+  }
+}
+export class dashboardStagingNavgbeu0qmzfiw5i {
+  page: Page;
+  span1wTGLWSoV1Field: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[normalize-space() = \"Assign to entity\"]`,
+  }
+  pqdhyy9UzBWField: MuukLocator = {
+    index: 0,
+    selector: `//P[normalize-space() = \"Select Entity Type\"]`,
+  }
+}
+export class googleComw1zlr5y53vu {
+  page: Page;
+  inputvqSzzS64Field: MuukLocator = {
+    index: 0,
+    selector: `//input[@aria-label=\"Google Search\"]`,
+  }
+}
