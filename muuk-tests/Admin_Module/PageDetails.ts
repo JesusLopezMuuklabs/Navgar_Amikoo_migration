54d7aa1229a0376d243f40699cb24fba99a0cf52
Jesus Lopez <jesus.lopez@muuklabs.com>
2026-09-06 20:14:11 -0600

    Admin Module added to repo

---
+++ muuk-tests/Admin_Module/PageDetails.ts
@@ -0,0 +1,425 @@
+import { expect, FrameLocator, Frame, Locator, Page } from '@playwright/test';
+import MuukLocator from './MuukLocator';
+//import utils from Utils;
+
+export class dashboardStagingNavgod25yq9dx43d {
+  page: Page;
+  h2VKUvVlcxo9Field: MuukLocator = {
+    index: 0,
+    selector: `//a[contains(text(),\"Log In\")]`,
+  }
+  inputaPC0oahLxOField: MuukLocator = {
+    index: 0,
+    selector: `INPUT[type='email'][name='user[email]'][id='user_email']`,
+  }
+  inputNoTndoCpdqField: MuukLocator = {
+    index: 0,
+    selector: `INPUT[type='password'][name='user[password]'][id='user_password']`,
+  }
+  inputPriEV9ualJField: MuukLocator = {
+    index: 0,
+    selector: `INPUT[type='submit'][name='commit']`,
+  }
+}
+export class dashboardStagingNavgkfsq0sqyxlg {
+  page: Page;
+  h190uQpSB1mField: MuukLocator = {
+    index: 0,
+    selector: `//H1[normalize-space() = \"Accounts\"]`,
+  }
+  tdxZFxlUx1zField: MuukLocator = {
+    index: 0,
+    selector: `//td[.//text()[contains(., 'MuukTest')]]`,
+  }
+  tdaKheGIQ7MField: MuukLocator = {
+    index: 1,
+    selector: `//td[.//text()[contains(., 'MuukTest')]]`,
+  }
+  imgXRULVUzNField: MuukLocator = {
+    index: 0,
+    selector: `//td//img[contains(@alt, 'MuukTest')]`,
+  }
+  amOUgGJtA6Field: MuukLocator = {
+    index: 0,
+    selector: `//tr[.//span[contains(text(), 'MuukTest - Feature Flag Environment')]]//a[@label='Switch']`,
+  }
+  a4JU9Y3c0KjField: MuukLocator = {
+    index: 0,
+    selector: `//A[normalize-space() = \"View\"]`,
+  }
+  spanb8cB3wfQY2Field: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[normalize-space() = \"MuukTest - Production Environment\"]`,
+  }
+}
+export class dashboardStagingNavgvjr1scz3vz9l {
+  page: Page;
+  divMNIPe7sX5DField: MuukLocator = {
+    index: 0,
+    selector: `//tr[.//span[contains(text(), 'MuukTest - Feature Flag Environment')]]//div[text()='Current Account']`,
+  }
+}
+export class dashboardStagingNavgdm2hlm5bgbh {
+  page: Page;
+  span5EhVAMC0wDField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[normalize-space() = \"MuukTest - Feature Flag Environment\"]`,
+  }
+  a4WT8aZfopKField: MuukLocator = {
+    index: 2,
+    selector: `//A[normalize-space() = \"Accounts\"]`,
+  }
+  ax2I5pZc9PDField: MuukLocator = {
+    index: 0,
+    selector: `//A[normalize-space() = \"Create an Account\"]`,
+  }
+  h1JZS2WsA3jField: MuukLocator = {
+    index: 0,
+    selector: `//H1[normalize-space() = \"Accounts\"]`,
+  }
+}
+export class dashboardStagingNavgs2xobrrm80o6 {
+  page: Page;
+  h1zQHD5IE79Field: MuukLocator = {
+    index: 0,
+    selector: `//H1[normalize-space() = \"MuukTest - Feature Flag Environment\"]`,
+  }
+  amjsYZDUy1xField: MuukLocator = {
+    index: 0,
+    selector: `//A[normalize-space() = \"Edit Account\"]`,
+  }
+  p2Zcq8NsP8Field: MuukLocator = {
+    index: 0,
+    selector: `//P[normalize-space() = \"Account was successfully updated.\"]`,
+  }
+  thQotobUzLLField: MuukLocator = {
+    index: 0,
+    selector: `//th[text() = \"User\"]`,
+  }
+  thzlHD7dhb33Field: MuukLocator = {
+    index: 0,
+    selector: `//th[text() = \"Friendly code\"]`,
+  }
+  thL6SOrQH9kyField: MuukLocator = {
+    index: 0,
+    selector: `//th[text() = \"Email\"]`,
+  }
+  thYEimXaiLzKField: MuukLocator = {
+    index: 0,
+    selector: `//th[text() = \"Phone number\"]`,
+  }
+  th8gTY2HQRyField: MuukLocator = {
+    index: 0,
+    selector: `//th[text() = \"Roles\"]`,
+  }
+  aBwI6bACl7AField: MuukLocator = {
+    index: 0,
+    selector: `//A[normalize-space() = \"Invite A User\"]`,
+  }
+  buttonG0t9AQu9k0Field: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[@type='submit'][normalize-space() = \"New Registration Code\"]`,
+  }
+  p8KSwpjiqWqField: MuukLocator = {
+    index: 0,
+    selector: `//P[contains(text(), \"New registration code created: \")]`,
+  }
+}
+export class dashboardStagingNavgp0y1vwxva1e8 {
+  page: Page;
+  labelYBhVrZuWy3Field: MuukLocator = {
+    index: 0,
+    selector: `//LABEL[normalize-space() = \"Account Name\"]`,
+  }
+  labelVRUJvrI8opField: MuukLocator = {
+    index: 0,
+    selector: `//LABEL[normalize-space() = \"Default language\"]`,
+  }
+  labelQbHR7uNE9Field: MuukLocator = {
+    index: 0,
+    selector: `//LABEL[normalize-space() = \"Friendly code\"]`,
+  }
+  buttonrqcVrKhzPxField: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[@name='button'][@type='submit'][normalize-space() = \"Update Account\"]`,
+  }
+  select8VI5DVsapAField: MuukLocator = {
+    index: 0,
+    selector: `SELECT[name='account[default_language]'][id='account_default_language']`,
+  }
+}
+export class dashboardStagingNavgeh7wsg0a2ub {
+  page: Page;
+  labelQYuw9LHZmrField: MuukLocator = {
+    index: 0,
+    selector: `//LABEL[normalize-space() = \"Name\"]`,
+  }
+  labelXeSKdliA6kField: MuukLocator = {
+    index: 0,
+    selector: `//LABEL[normalize-space() = \"Email\"]`,
+  }
+  buttonOrCOr5I5OrField: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[@name='button'][@type='submit'][normalize-space() = \"Send invitation\"]`,
+  }
+}
+export class dashboardStagingNavgupzppbviuhmp {
+  page: Page;
+  spandBsGveI6J1Field: MuukLocator = {
+    index: 0,
+    selector: `//div[@role=\"button\"][@aria-label=\"Profile Menu\"]`,
+  }
+  a0DFVpQilnbField: MuukLocator = {
+    index: 1,
+    selector: `//A[normalize-space() = \"Accounts\"]`,
+  }
+  spanaEUx60TWG2Field: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[normalize-space() = \"Applications\"]`,
+  }
+}
+export class dashboardStagingNavgkoyr3hqnctqq {
+  page: Page;
+  labelnEie8buqWbField: MuukLocator = {
+    index: 0,
+    selector: `//LABEL[normalize-space() = \"Account Name\"]`,
+  }
+  labelnmFnJ3T6bRField: MuukLocator = {
+    index: 0,
+    selector: `//select[@id=\"account_default_language\"]`,
+  }
+  img3ZtbAgTvYField: MuukLocator = {
+    index: 0,
+    selector: `//label[@for='account_avatar']/following-sibling::div//img`,
+  }
+  aqeH2YlU7kvField: MuukLocator = {
+    index: 0,
+    selector: `//A[normalize-space() = \"Delete\"]`,
+  }
+  h5LZCQ5mUMMlField: MuukLocator = {
+    index: 0,
+    selector: `//h5[\"Are you sure?\"]`,
+  }
+  buttonogi3OPHDx3Field: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[@name='button'][@type='submit'][normalize-space() = \"Update Account\"]`,
+  }
+  selectQPHJlHV1MField: MuukLocator = {
+    index: 0,
+    selector: `SELECT[name='account[default_language]'][id='account_default_language']`,
+  }
+  input8jt5w2HkwGField: MuukLocator = {
+    index: 0,
+    selector: `INPUT[type='file'][name='account[avatar]'][id='account_avatar']`,
+  }
+  aPESLyJCJLField: MuukLocator = {
+    index: 1,
+    selector: `//A[normalize-space() = \"MuukTest - Production Environment\"]`,
+  }
+  imgMDT30yIZw4Field: MuukLocator = {
+    index: 0,
+    selector: `//h1[.//a[contains(text(), 'MuukTest - Production')]]/preceding-sibling::img`,
+  }
+}
+export class dashboardStagingNavguciflemiatuk {
+  page: Page;
+  pn0IlylI19jField: MuukLocator = {
+    index: 0,
+    selector: `//P[normalize-space() = \"Account was successfully updated.\"]`,
+  }
+  adrURdaHYBField: MuukLocator = {
+    index: 0,
+    selector: `//A[normalize-space() = \"Edit Account\"]`,
+  }
+}
+export class dashboardStagingNavgidqm7qb1ce54 {
+  page: Page;
+  ar6cElgHXYField: MuukLocator = {
+    index: 0,
+    selector: `//A[normalize-space() = \"Edit\"]`,
+  }
+}
+export class dashboardStagingNavgt8hiolkacpo {
+  page: Page;
+  h1DDseMYt9GField: MuukLocator = {
+    index: 0,
+    selector: `//H1[normalize-space() = \"Accounts Icons/cheveron right New Account\"]`,
+  }
+  labelQRu72Gi0KField: MuukLocator = {
+    index: 0,
+    selector: `//LABEL[normalize-space() = \"Account Name\"]`,
+  }
+  label8quqOgs47Field: MuukLocator = {
+    index: 0,
+    selector: `//LABEL[normalize-space() = \"Default language\"]`,
+  }
+  buttong3TnpIOWZField: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[@name='button'][@type='submit'][normalize-space() = \"Create Account\"]`,
+  }
+}
+export class dashboardStagingNavgkat21kg28a4 {
+  page: Page;
+  aAru7e8TVR8Field: MuukLocator = {
+    index: 1,
+    selector: `//A[normalize-space() = \"Profile\"]`,
+  }
+  h1nbfpLaUqbZField: MuukLocator = {
+    index: 0,
+    selector: `//H1[normalize-space() = \"Edit Profile\"]`,
+  }
+  span1ZFD4rhgyField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[normalize-space() = \"Update\"]`,
+  }
+  input6psKrliB8pField: MuukLocator = {
+    index: 0,
+    selector: `INPUT[placeholder='SJ'][type='text'][name='user[initials]'][id='user_initials']`,
+  }
+  inputnBhOx3i8y0Field: MuukLocator = {
+    index: 0,
+    selector: `INPUT[type='file'][name='user[avatar]'][id='user_avatar']`,
+  }
+}
+export class dashboardStagingNavg90djub1wboef {
+  page: Page;
+  labelvwtZyt5OvField: MuukLocator = {
+    index: 0,
+    selector: `//LABEL[normalize-space() = \"Time zone\"]`,
+  }
+  labelVpCO5OSkqField: MuukLocator = {
+    index: 0,
+    selector: `//LABEL[normalize-space() = \"Preferred language\"]`,
+  }
+  labelZLT0ecwdKField: MuukLocator = {
+    index: 0,
+    selector: `//LABEL[normalize-space() = \"Phone numbers\"]`,
+  }
+  aV7U9r4CRYField: MuukLocator = {
+    index: 0,
+    selector: `//A[normalize-space() = \"Add phone\"]`,
+  }
+  spanGWtyMgfdjField: MuukLocator = {
+    index: 0,
+    selector: `//SPAN[normalize-space() = \"Update\"]`,
+  }
+  buttonrJtvgqI73GField: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[@type='submit'][normalize-space() = \"Delete my account\"]`,
+  }
+  h5gvcwGyTn9Field: MuukLocator = {
+    index: 0,
+    selector: `//h5[normalize-space() = 'Are you sure?']`,
+  }
+  button55fL4MUJzField: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[normalize-space() = \"Cancel\"]`,
+  }
+  input6wsjp2IV7Field: MuukLocator = {
+    index: 0,
+    selector: `INPUT[type='text'][id=\"user_phone_numbers_attributes_1788745416652_value\"]`,
+  }
+}
+export class dashboardStagingNavgoy1kmmmd2m {
+  page: Page;
+  h2OPt321FaNXField: MuukLocator = {
+    index: 0,
+    selector: `//H2[normalize-space() = \"Two-factor Authentication\"]`,
+  }
+  aebtA06tQg3Field: MuukLocator = {
+    index: 0,
+    selector: `//A[normalize-space() = \"Enable two-factor authentication\"]`,
+  }
+  h14GHEjBuozgField: MuukLocator = {
+    index: 0,
+    selector: `//H1[normalize-space() = \"Update Password\"]`,
+  }
+  label73fjNohorField: MuukLocator = {
+    index: 0,
+    selector: `//LABEL[normalize-space() = \"Current password\"]`,
+  }
+  labelpys362cgl0Field: MuukLocator = {
+    index: 0,
+    selector: `//LABEL[normalize-space() = \"New password\"]`,
+  }
+  labelBvU2nuRaField: MuukLocator = {
+    index: 0,
+    selector: `//LABEL[normalize-space() = \"New password confirmation\"]`,
+  }
+  agwu6DohvjzField: MuukLocator = {
+    index: 1,
+    selector: `//A[normalize-space() = \"Password\"]`,
+  }
+  abDT6VkSZgbField: MuukLocator = {
+    index: 1,
+    selector: `//A[normalize-space() = \"Connected Accounts\"]`,
+  }
+  inputPdI4rNimQEField: MuukLocator = {
+    index: 0,
+    selector: `INPUT[type='submit'][name='commit']`,
+  }
+}
+export class dashboardStagingNavgkclsmnhqvhz {
+  page: Page;
+  h3OJT3l0cPPoField: MuukLocator = {
+    index: 0,
+    selector: `//H3[normalize-space() = \"Backup Codes\"]`,
+  }
+  aKwhrJ4t0A6Field: MuukLocator = {
+    index: 0,
+    selector: `//A[normalize-space() = \"Cancel\"]`,
+  }
+}
+export class dashboardStagingNavgybvw8equak2q {
+  page: Page;
+  h2oWtq9hqvField: MuukLocator = {
+    index: 0,
+    selector: `//H2[normalize-space() = \"Connected Accounts\"]`,
+  }
+  buttonXeHZWrSj5GField: MuukLocator = {
+    index: 0,
+    selector: `//button[@class=\"btn btn-google_oauth2\"]`,
+  }
+  ax1CgVxF1pnField: MuukLocator = {
+    index: 1,
+    selector: `//A[normalize-space() = \"Connected Accounts\"]`,
+  }
+  buttonxWHEaOUjyoField: MuukLocator = {
+    index: 0,
+    selector: `//BUTTON[@type='submit'][normalize-space() = \"MS 365\"]`,
+  }
+}
+export class w3schoolsComafj2me66hboa {
+  page: Page;
+  a2YT2BAsqv9Field: MuukLocator = {
+    index: 0,
+    selector: `//input[@value=\"MuukTest - Production Environment \"]`,
+  }
+  agKTyKGtYEWField: MuukLocator = {
+    index: 0,
+    selector: `//input[@value=\"AR\"]`,
+  }
+  a5ocmZ5IQkDField: MuukLocator = {
+    index: 0,
+    selector: `//input[@value=\"ASD\"]`,
+  }
+}
+export class developerMozillaOrgxzdbdgwwbnu4 {
+  page: Page;
+  liyec1Nji97aField: MuukLocator = {
+    index: 0,
+    selector: `//option`,
+  }
+  liQ8ua23fRjmField: MuukLocator = {
+    index: 1,
+    selector: `//option`,
+  }
+  liyytvvAFbVGField: MuukLocator = {
+    index: 0,
+    selector: `//button[\"Confirm\"][@data-behavior=\"commit\"]`,
+  }
+  lizPT9EvsaQLField: MuukLocator = {
+    index: 0,
+    selector: `//button[\"Cancel\"][@data-behavior=\"cancel\"]`,
+  }
+}
